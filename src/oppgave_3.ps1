c4e7758b0c829a00ed05c1895ca8e4c9a0e7fa5b
Harald Groseth <harald.groseth@nav.no>
2022-05-04 13:23:29 +0200

    oppgave 3

---
+++ src/oppgave_3.ps1
@@ -0,0 +1,40 @@
+$ErrorActionPreference = 'Stop'
+
+$webRequest = Invoke-WebRequest -Uri "http://nav-deckofcards.herokuapp.com/shuffle"
+
+
+$kortstokkJson = $webRequest.Content
+
+# se ./src/hints/kortstokk.json for formatert utgave
+
+$kortstokk = ConvertFrom-Json -InputObject $kortstokkJson
+
+
+# 1. utgave - foreach loop som skriver ut et kort per linje
+foreach ($kort in $kortstokk) {
+    Write-Output $kort
+}
+
+# 2. utgave - interessert i 1. karakter i merke - (S)PADE - og verdi
+foreach ($kort in $kortstokk) {
+    Write-Output "$($kort.suit[0])$($kort.value)"
+}
+
+# 3. utgave - ønsker egentlig hele kortstokken som en streng og den koden som en funksjon (gjenbruk)
+
+function kortstokkTilStreng {
+    [OutputType([string])]
+    param (
+        [object[]]
+        $kortstokk
+    )
+    $streng = ""
+    foreach ($kort in $kortstokk) {
+        $streng = $streng + "$($kort.suit[0])" + $kort.value + ","
+    }
+    return $streng
+}
+
+
+Write-Output "Kortstokk: $(kortStokkTilStreng -kortstokk $kortstokk)"
+
